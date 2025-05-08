2b35ac77f9b78ddfdf52bc7b1656da217dcc4a56
rs22095L.nago4 <120536850+nago4@users.noreply.github.com>
2025-05-08 13:37:43 +0900

    スクロールバーの追加

--- time.js
+++ time.js
@@ -9,3 +9,13 @@ if(hour >= 4　&& hour < 11){
     }else{
     document.getElementById('greeting').textContent ="こんばんは！";
     }
+
+//スクロールバーの進捗を取得する関数
+const getScrollPercent = () => {
+    const scrolled = window.scrollY;
+    const pageHeight = document.documentElement.scrollHeight;
+    const viewHeight = document.documentElement.clientHeight;
+    const percentage = scrolled / (pageHeight - viewHeight) * 100;
+    document.querySelector("#bar").style.width = `${percentage}%`;
+};
+window.addEventListener('scroll', getScrollPercent);
